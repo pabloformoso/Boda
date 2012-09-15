a036641949c3b362b79c1342cc984ee990d4a989
Pablo Formoso Estrada <pablo@pabloformoso.com>
2012-09-15 11:45:04 +0200

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boda::Application.config.secret_token = '3061dcbc626dcb10bc37044296de72aacebdccc4eeb8771110384ae26ada3dd0c533bb51ca343d4975dabfff5c58dcfdbfb8a5ac7d7a314c5f97793da26b808f'
