c97e46841aecea63641c57a8b9e539c6e697dad4
Pablo Formoso Estrada <pablo@pabloformoso.com>
2012-09-19 22:40:10 +0200

    admin

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,5 +1,12 @@
 class BaseController < ApplicationController
   layout "user"
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :days
+  
+  def days
+    a = Time.gm(2012,12,01)
+    b = Time.now
+    
+    @days = (a - b).to_i / (24 * 60 * 60)
+  end
   
 end
--- app/models/admin/guest.rb
+++ app/models/admin/guest.rb
@@ -1,4 +1,6 @@
 class Admin::Guest < ActiveRecord::Base
   belongs_to :user, :class_name => "User", :foreign_key => "user_id"
   attr_accessible :city, :has_confirmed, :lastname, :name, :number_of_companions, :user
+  
+  scope :confirmed, where(:has_confirmed => true)
 end
