cd147bde59efd224af00ac971560888ee38fa596
Pablo Formoso Estrada <pablo@pabloformoso.com>
2012-10-05 16:18:27 +0200

    v2

--- app/models/admin/access_request.rb
+++ app/models/admin/access_request.rb
@@ -1,6 +1,7 @@
 class Admin::AccessRequest < ActiveRecord::Base
 
   attr_accessible :email, :name, :revised
-  
+  validates :email, :uniqueness => true, :on => :create
+
   scope :not_revised, where(:revised => false) 
 end
